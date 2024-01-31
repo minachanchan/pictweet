747996c9460c4998a7061dbabca628e2475ce020
minachanchan <ayaka.minagawa@di-v.co.jp>
2024-01-31 19:02:43 +0900

    SecurityConfigクラスの作成・設定

--- src/main/java/in/techcamp/pictweet/UserRepository.java
+++ src/main/java/in/techcamp/pictweet/UserRepository.java
@@ -3,4 +3,5 @@ package in.techcamp.pictweet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    UserEntity findByUsername(String username);
 }
