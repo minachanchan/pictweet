13e9d8898b0e2a25e13dfc1c806c81fce360ac35
minachanchan <ayaka.minagawa@di-v.co.jp>
2024-02-04 02:06:59 +0900

    詳細機能・詳細画面の実装

--- src/main/java/in/techcamp/pictweet/TweetController.java
+++ src/main/java/in/techcamp/pictweet/TweetController.java
@@ -2,6 +2,7 @@ package in.techcamp.pictweet;
 
 //import lombok.RequiredArgsConstructor;
 //import org.apache.catalina.User;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -74,4 +76,25 @@ public class TweetController {
 //        tweetRepository.insertTweet(form.getContent(), form.getImage());
 //        return "redirect:/";
 //    }
+
+   @GetMapping("/tweet/{tweetId}")
+    public String showTweetDetail(@PathVariable("tweetId") Integer tweetId, Model model){
+        TweetEntity tweet;
+
+        try {
+            tweet = tweetRepository.findBy(tweetId).orElseThrow(() -> new EntityNotFoundException("Tweet not found:" + tweetId));
+        } catch (EntityNotFoundException ex){
+            model.addAttribute("errorMessage", ex.getMessage());
+            return "error";
+        }
+
+        model.addAttribute("t", tweet);
+        return  "detail";
+   }
+
+
+
+
+
+
 }
