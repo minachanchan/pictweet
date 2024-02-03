8b89b8a662c236ed3db1e41af81704a0d207219f
minachanchan <ayaka.minagawa@di-v.co.jp>
2024-02-04 01:38:34 +0900

    Userのインポートステートメントの修正

--- src/main/java/in/techcamp/pictweet/TweetController.java
+++ src/main/java/in/techcamp/pictweet/TweetController.java
@@ -1,9 +1,10 @@
 package in.techcamp.pictweet;
 
-import lombok.RequiredArgsConstructor;
-import org.apache.catalina.User;
+//import lombok.RequiredArgsConstructor;
+//import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,7 @@ import java.util.List;
 //import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class TweetController {
 //    private final TweetRepository tweetRepository;
     @Autowired
