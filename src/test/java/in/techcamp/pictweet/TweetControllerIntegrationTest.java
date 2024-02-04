e5e6a6287ea13ef36ca769d4266cee59e7a677ea
minachanchan <ayaka.minagawa@di-v.co.jp>
2024-02-04 20:07:23 +0900

    MockMvcの設定

---
+++ src/test/java/in/techcamp/pictweet/TweetControllerIntegrationTest.java
@@ -0,0 +1,14 @@
+package in.techcamp.pictweet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TweetControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+}
