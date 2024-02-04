a5458a91b4ac32d2d0c10d48c012ef42a8d110cd
minachanchan <ayaka.minagawa@di-v.co.jp>
2024-02-04 16:07:31 +0900

    代用モックとテスト対象の定義

--- src/test/java/in/techcamp/pictweet/TweetControllerUnitTest.java
+++ src/test/java/in/techcamp/pictweet/TweetControllerUnitTest.java
@@ -1,8 +1,19 @@
 package in.techcamp.pictweet;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class IssueControllerUnitTest {
+public class TweetControllerUnitTest {
+
+    @Mock
+    private TweetRepository tweetRepository;
+
+    @Mock
+    private CommentRepository commentRepository;
+
+    @InjectMocks
+    private TweetController tweetController = new TweetController();
 }
