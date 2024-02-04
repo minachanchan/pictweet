25b28ea2ad3cd0dea2a11c6f315146ed1b6f3be1
minachanchan <ayaka.minagawa@di-v.co.jp>
2024-02-04 16:17:56 +0900

    テスト実装

--- src/test/java/in/techcamp/pictweet/TweetControllerUnitTest.java
+++ src/test/java/in/techcamp/pictweet/TweetControllerUnitTest.java
@@ -1,9 +1,21 @@
 package in.techcamp.pictweet;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class TweetControllerUnitTest {
@@ -16,4 +28,26 @@ public class TweetControllerUnitTest {
 
     @InjectMocks
     private TweetController tweetController = new TweetController();
+
+    @Test
+    public void testShowTweetDetail() {
+        TweetEntity expectedTweet = new TweetEntity();
+
+        List<CommentEntity> expectedComments = new ArrayList<>();
+
+        CommentEntity comment1 = new CommentEntity();
+        comment1.setId(1);
+        comment1.setMessage("This is a comment.");
+        expectedComments.add(comment1);
+
+        when(tweetRepository.findById(any(Integer.class))).thenReturn(Optional.of(expectedTweet));
+        when(commentRepository.findByTweet_id(any(Integer.class))).thenReturn(expectedComments);
+
+        Model model = new ExtendedModelMap();
+        String result = tweetController.showTweetDetail(1, new CommentEntity(), model);
+
+        assertThat(result, is("detail"));
+        assertThat(model.getAttribute("issue"), is(expectedTweet));
+        assertThat(model.getAttribute("comments"), is(expectedComments));
+    }
 }
