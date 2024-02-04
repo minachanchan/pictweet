c1200b25e0a048c319032b1603fce6626064fb29
minachanchan <ayaka.minagawa@di-v.co.jp>
2024-02-04 16:05:23 +0900

    Mockito使用準備

---
+++ src/test/java/in/techcamp/pictweet/TweetControllerUnitTest.java
@@ -0,0 +1,8 @@
+package in.techcamp.pictweet;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class IssueControllerUnitTest {
+}
