1f61c29df66775e025d58fe66d8a4618873e1a42
minachanchan <ayaka.minagawa@di-v.co.jp>
2024-02-07 06:45:41 +0900

    テストの無効化

--- src/test/java/in/techcamp/pictweet/LoginTest.java
+++ src/test/java/in/techcamp/pictweet/LoginTest.java
@@ -1,30 +1,30 @@
-package in.techcamp.pictweet;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-
-@SpringBootTest
-@AutoConfigureMockMvc
-public class LoginTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    public void testSuccessfulLogin() throws Exception {
+//package in.techcamp.pictweet;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+//
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//public class LoginTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Test
+//    public void testSuccessfulLogin() throws Exception {
 //        mockMvc.perform(post("/login")
 //                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
 //                        .param("testa", "---ご自身のDBに登録されているユーザーネームを入力してください---")
 //                        .param("aaa111", "---上で登録したユーザーに紐づくパスワードを入力してください---"))
 //                .andExpect(status().is3xxRedirection())
 //                .andExpect(redirectedUrl("/"));
-    }
-}
+//    }
+//}
